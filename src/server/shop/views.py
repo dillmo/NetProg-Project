6dd902c638e45ea21626a0cd2dc8442dfac3037f
connollyp <43173642+connollyp@users.noreply.github.com>
2018-11-17 16:30:56 -0500

    Update views.py

--- src/server/shop/views.py
+++ src/server/shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http      import HttpResponse, HttpResponseRedirect
 from django.urls      import reverse
 
+my_cart = request.session.get('my_cart')
+
 def index(request):
     context = {}
     return render(request, 'shop/index.html', context)
@@ -25,31 +27,10 @@ def register_user(request):
     return HttpResponseRedirect(reverse('index'))
 
 def addToCart(request, product_id, color, size):
-    return HttpResponse("cart")
-
-def cart(request):
-    context = {
-        'product_id':   request.POST['product_id],
-        'color':        request.POST['color'],
-        'size':         request.POST['size']
-    }
-    return render(request, 'cart/index.html', context)
-                                     
-def checkout(request):
-    context = {
-        'name':         request.POST['name'],
-        'email':        request.POST['email'],
-        'phone':        request.POST['phone'],
-        'address':      request.POST['address'],
-        'apt_num':      request.POST['apt_num'],
-        'zip':          request.POST['zip'],
-        'city':         request.POST['city'],
-        'state':        request.POST['state'],
-        'country':      request.POST['country'],
-        'card_num':     request.POST['card_num'],
-        'exp_day':      request.POST['exp_day'],
-        'exp_year':     request.POST['exp_year'],
-        'cvv':          request.POST['cvv']
-    }
-    return render(request, 'checkout/index.html', context)
+    my_cart = request.session.push('my_cart', product_id, color, size)
+    context = {}
+    return render(request, 'shop/cart.html', context)
+                              
+def checkout(request, name, email, phone, address, apt_num, zip_code, city, state, country, card_num, exp_day, exp_year, cvv):
+    
     
