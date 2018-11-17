17843e4878560b4d96c332522efc7a1f9036f429
connollyp <43173642+connollyp@users.noreply.github.com>
2018-11-17 16:03:01 -0500

    Update Views.py

--- src/server/shop/views.py
+++ src/server/shop/views.py
@@ -23,3 +23,33 @@ def register_user(request):
         'password': request.POST['password']
     }
     return HttpResponseRedirect(reverse('index'))
+
+def addToCart(request, product_id, color, size):
+    return HttpResponse("cart")
+
+def cart(request):
+    context = {
+        'product_id':   request.POST['product_id],
+        'color':        request.POST['color'],
+        'size':         request.POST['size']
+    }
+    return render(request, 'cart/index.html', context)
+                                     
+def checkout(request):
+    context = {
+        'name':         request.POST['name'],
+        'email':        request.POST['email'],
+        'phone':        request.POST['phone'],
+        'address':      request.POST['address'],
+        'apt_num':      request.POST['apt_num'],
+        'zip':          request.POST['zip'],
+        'city':         request.POST['city'],
+        'state':        request.POST['state'],
+        'country':      request.POST['country'],
+        'card_num':     request.POST['card_num'],
+        'exp_day':      request.POST['exp_day'],
+        'exp_year':     request.POST['exp_year'],
+        'cvt':          request.POST['cvt']
+    }
+    return render(request, 'checkout/index.html', context)
+    
