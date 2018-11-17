9140457ff4a5716617b2ca5e0e10161d98f86364
connollyp <43173642+connollyp@users.noreply.github.com>
2018-11-17 16:02:13 -0500

    Update urls.py

--- src/server/shop/urls.py
+++ src/server/shop/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('register/success', views.register_user, name='register_user'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
+    path('checkout/success', views.checkout_complete, name='checkout_complete')
 ]
